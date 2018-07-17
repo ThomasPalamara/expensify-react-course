c47eb40b2340c82fc2b7d1d5a47d56eba90a8cca
ThomasP-Weblimitless <thomas@algbmail.com>
2018-07-17 11:22:45 +0100

    Remove test expenses

--- src/app.js
+++ src/app.js
@@ -10,12 +10,6 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 700 }))
-store.dispatch(addExpense({ description: 'water bill', amount: 300 }))
-store.dispatch(addExpense({ description: 'rent', amount: 108500 }))
-
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
